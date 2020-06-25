a7523b253212dd47abca5eba1d4ab2cb196148d1
Salma Ghoneim <salmaghoneim97@gmail.com>
2020-06-25 23:17:15 +0200

    Updates

---
+++ precache-manifest.ebf0fdd73d9ab0da30e760b197228625.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e5c85149d0fca7f7e21d557c716bd7b",
+    "url": "/Nodogoro-task/index.html"
+  },
+  {
+    "revision": "aec23f364da5b89b5d71",
+    "url": "/Nodogoro-task/static/js/2.5a7fdfd3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Nodogoro-task/static/js/2.5a7fdfd3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "988e32aed1ce17ff8cdc",
+    "url": "/Nodogoro-task/static/js/main.453d9c1d.chunk.js"
+  },
+  {
+    "revision": "57920c5f736d573556d1",
+    "url": "/Nodogoro-task/static/js/runtime-main.2c618223.js"
+  },
+  {
+    "revision": "39c53a5a2c3f4e89bf0fddb583c8f8d1",
+    "url": "/Nodogoro-task/static/media/burger.39c53a5a.png"
+  },
+  {
+    "revision": "21f0e8a73b2fd4970bce4ed966887f52",
+    "url": "/Nodogoro-task/static/media/logo-pic-only.21f0e8a7.png"
+  },
+  {
+    "revision": "49d7eb4963a5206336f03b533646732a",
+    "url": "/Nodogoro-task/static/media/logo.49d7eb49.png"
+  },
+  {
+    "revision": "f81ff4668c45c0fe37a68d2c3a42d9f9",
+    "url": "/Nodogoro-task/static/media/pasta.f81ff466.png"
+  },
+  {
+    "revision": "347be373c71fc57b997b26ec56999810",
+    "url": "/Nodogoro-task/static/media/pizza.347be373.png"
+  },
+  {
+    "revision": "a46dd3341141113a654810e598368317",
+    "url": "/Nodogoro-task/static/media/sandwich.a46dd334.jpg"
+  }
+]);
